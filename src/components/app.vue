b72ce3e6f94d6d3a2407b0b7469c05b31b8e32f2
WARMINDO1 <muhammad.naufalr07@gmail.com>
2022-07-18 00:21:01 +0700

    Signin page dan Signup page

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,31 +1,27 @@
 <template>
   <f7-app v-bind="f7params" dark>
+    <!-- Left panel with cover effect-->
+    <f7-panel left cover dark>
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Left Panel"></f7-navbar>
+          <f7-block>Left panel content goes here</f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
 
-  <!-- Left panel with cover effect-->
-  <f7-panel left cover dark>
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Left Panel"></f7-navbar>
-        <f7-block>Left panel content goes here</f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-panel>
+    <!-- Right panel with reveal effect-->
+    <f7-panel right reveal dark>
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Right Panel"></f7-navbar>
+          <f7-block>Right panel content goes here</f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
 
-
-  <!-- Right panel with reveal effect-->
-  <f7-panel right reveal dark>
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Right Panel"></f7-navbar>
-        <f7-block>Right panel content goes here</f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-panel>
-
-
-  <!-- Your main view, should have "view-main" class -->
-  <f7-view main class="safe-areas" url="/"></f7-view>
-
+    <!-- Your main view, should have "view-main" class -->
+    <f7-view main class="safe-areas" :push-state="true"></f7-view>
 
     <!-- Popup -->
     <f7-popup id="my-popup">
@@ -48,24 +44,12 @@
         <f7-page login-screen>
           <f7-login-screen-title>Login</f7-login-screen-title>
           <f7-list form>
-            <f7-list-input
-              type="text"
-              name="username"
-              placeholder="Your username"
-              v-model:value="username"
-            ></f7-list-input>
-            <f7-list-input
-              type="password"
-              name="password"
-              placeholder="Your password"
-              v-model:value="password"
-            ></f7-list-input>
+            <f7-list-input type="text" name="username" placeholder="Your username" v-model:value="username"></f7-list-input>
+            <f7-list-input type="password" name="password" placeholder="Your password" v-model:value="password"></f7-list-input>
           </f7-list>
           <f7-list>
             <f7-list-button title="Sign In" @click="alertLoginData"></f7-list-button>
-            <f7-block-footer>
-              Some text about login information.<br>Click "Sign In" to close Login Screen
-            </f7-block-footer>
+            <f7-block-footer> Some text about login information.<br />Click "Sign In" to close Login Screen </f7-block-footer>
           </f7-list>
         </f7-page>
       </f7-view>
@@ -73,67 +57,65 @@
   </f7-app>
 </template>
 <script>
-  import { ref, onMounted } from 'vue';
-  import { f7, f7ready } from 'framework7-vue';
-  import { getDevice }  from 'framework7/lite-bundle';
-  import cordovaApp from '../js/cordova-app.js';
+import { ref, onMounted } from "vue";
+import { f7, f7ready } from "framework7-vue";
+import { getDevice } from "framework7/lite-bundle";
+import cordovaApp from "../js/cordova-app.js";
 
-  import routes from '../js/routes.js';
-  import store from '../js/store';
+import routes from "../js/routes.js";
+import store from "../js/store";
 
-  export default {
-    setup() {
-      const device = getDevice();
-      // Framework7 Parameters
-      const f7params = {
-        name: 'My App', // App name
-        theme: 'auto', // Automatic theme detection
+export default {
+  setup() {
+    const device = getDevice();
+    // Framework7 Parameters
+    const f7params = {
+      name: "My App", // App name
+      theme: "auto", // Automatic theme detection
 
+      id: "io.framework7.myapp", // App bundle ID
+      // App store
+      store: store,
+      // App routes
+      routes: routes,
 
-        id: 'io.framework7.myapp', // App bundle ID
-        // App store
-        store: store,
-        // App routes
-        routes: routes,
+      // Input settings
+      input: {
+        scrollIntoViewOnFocus: device.cordova && !device.electron,
+        scrollIntoViewCentered: device.cordova && !device.electron,
+      },
+      // Cordova Statusbar settings
+      statusbar: {
+        iosOverlaysWebView: true,
+        androidOverlaysWebView: false,
+      },
+    };
+    // Login screen data
+    const username = ref("");
+    const password = ref("");
 
-
-        // Input settings
-        input: {
-          scrollIntoViewOnFocus: device.cordova && !device.electron,
-          scrollIntoViewCentered: device.cordova && !device.electron,
-        },
-        // Cordova Statusbar settings
-        statusbar: {
-          iosOverlaysWebView: true,
-          androidOverlaysWebView: false,
-        },
-      };
-      // Login screen data
-      const username = ref('');
-      const password = ref('');
-
-      const alertLoginData = () => {
-        f7.dialog.alert('Username: ' + username.value + '<br>Password: ' + password.value, () => {
-          f7.loginScreen.close();
-        });
-      }
-      onMounted(() => {
-        f7ready(() => {
-          // Init cordova APIs (see cordova-app.js)
-          if (device.cordova) {
-            cordovaApp.init(f7);
-          }
-
-          // Call F7 APIs here
-        });
+    const alertLoginData = () => {
+      f7.dialog.alert("Username: " + username.value + "<br>Password: " + password.value, () => {
+        f7.loginScreen.close();
       });
+    };
+    onMounted(() => {
+      f7ready(() => {
+        // Init cordova APIs (see cordova-app.js)
+        if (device.cordova) {
+          cordovaApp.init(f7);
+        }
 
-      return {
-        f7params,
-        username,
-        password,
-        alertLoginData
-      }
-    }
-  }
+        // Call F7 APIs here
+      });
+    });
+
+    return {
+      f7params,
+      username,
+      password,
+      alertLoginData,
+    };
+  },
+};
 </script>
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,34 +1,41 @@
+import HomePage from "../pages/home.vue";
+import AboutPage from "../pages/about.vue";
+import FormPage from "../pages/form.vue";
+import SigninPage from "../pages/signin.vue";
+import SignupPage from "../pages/signup.vue";
 
-import HomePage from '../pages/home.vue';
-import AboutPage from '../pages/about.vue';
-import FormPage from '../pages/form.vue';
-
-
-import DynamicRoutePage from '../pages/dynamic-route.vue';
-import RequestAndLoad from '../pages/request-and-load.vue';
-import NotFoundPage from '../pages/404.vue';
+import DynamicRoutePage from "../pages/dynamic-route.vue";
+import RequestAndLoad from "../pages/request-and-load.vue";
+import NotFoundPage from "../pages/404.vue";
 
 var routes = [
   {
-    path: '/',
+    path: "/",
     component: HomePage,
   },
   {
-    path: '/about/',
+    path: "/register/",
+    component: SignupPage,
+  },
+  {
+    path: "/login/",
+    component: SigninPage,
+  },
+  {
+    path: "/about/",
     component: AboutPage,
   },
   {
-    path: '/form/',
+    path: "/form/",
     component: FormPage,
   },
 
-
   {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    path: "/dynamic-route/blog/:blogId/post/:postId/",
     component: DynamicRoutePage,
   },
   {
-    path: '/request-and-load/user/:userId/',
+    path: "/request-and-load/user/:userId/",
     async: function ({ router, to, resolve }) {
       // App instance
       var app = router.app;
@@ -43,19 +50,19 @@ var routes = [
       setTimeout(function () {
         // We got user data from request
         var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          firstName: "Vladimir",
+          lastName: "Kharlampidi",
+          about: "Hello, i am creator of Framework7! Hope you like it!",
           links: [
             {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
+              title: "Framework7 Website",
+              url: "http://framework7.io",
             },
             {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
+              title: "Framework7 Forum",
+              url: "http://forum.framework7.io",
             },
-          ]
+          ],
         };
         // Hide Preloader
         app.preloader.hide();
@@ -68,14 +75,14 @@ var routes = [
           {
             props: {
               user: user,
-            }
+            },
           }
         );
       }, 1000);
     },
   },
   {
-    path: '(.*)',
+    path: "(.*)",
     component: NotFoundPage,
   },
 ];
